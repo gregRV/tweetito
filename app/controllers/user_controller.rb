04f574e7627a8324d1b29f87244d481bb3ef0e11
Greg RV <gregvarias@gmail.com>
2014-09-09 23:03:27 -0700

    user can now sign up, view show page, and view all existing users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
 	def index
+		@users = User.all
 	end
 
 	def new
@@ -7,9 +8,17 @@ class UserController < ApplicationController
 	end
 
 	def create
-		puts '*'*80
-		puts params
-		flash[:notice] = 'saved ok!'
-		redirect_to :action => 'new'
+		user = User.create(user_params)
+		redirect_to user_path(user)
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
