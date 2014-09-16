d25f600c1e55506ca791dec09d1c4b4daf457cab
Greg RV <gregvarias@gmail.com>
2014-09-15 23:38:06 -0700

    added helper methods and updated session controller to use methods provided by has_secure_password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,13 @@ class SessionsController < ApplicationController
 	def create
 		ap "INSIDE SESSIONS#CREATE!!"
 		ap params
-		user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-		if user
+		# The line below works, and will return either false, or the User record
+		# user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+
+		# Here is a solution from RailsCasts using #authenticate, which is
+		# an instance method provided by has_secure_password
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to user, notice: "SUCCESS! FOUND USER"
 		else
